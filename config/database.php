c5c21d4a67603ebc5d163c4fa3c4e5395164d03c
abdiyfatah <abdiyfatah@email.com>
2026-04-24 05:17:31 +0300

    Move SQLite to storage folder

--- config/database.php
+++ config/database.php
@@ -33,15 +33,11 @@ return [
     'connections' => [
 
         'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
+            'driver'   => 'sqlite',
+            'url'      => env('DB_URL'),
+            'database' => env('DB_DATABASE', storage_path('database.sqlite')),
+            'prefix'   => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            'busy_timeout' => null,
-            'journal_mode' => null,
-            'synchronous' => null,
-            'transaction_mode' => 'DEFERRED',
         ],
 
         'mysql' => [
